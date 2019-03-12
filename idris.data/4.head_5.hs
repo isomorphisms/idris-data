7da9391ec176b32902b7553f472296d01b2a02ed
isomorphisms <isomorphisms@sdf.org>
2019-03-12 06:10:24 +0000

    fixed -5 on head

---
+++ idris.data/4.head_5.hs
@@ -0,0 +1,24 @@
+main :: IO ()
+main = do
+  ear <- getLine
+  let rational_brain = lines ear
+  print $ unlines $ take 5 $ rational_brain
+
+
+-- `lines` turns "string\nstrang\nstrung"
+--         into [string,strang,strung]
+-- `unlines` puts the newlines back
+
+
+-- for printing purposes this works like
+--        perl's convenient 'say'
+--        or ruby's convenient 'puts'
+
+-- `print` alone would put nothing between the arrays.
+
+
+
+-- (what if you wanted to \0 null-byte separate them?
+--                      …  or separate them for TCP?
+--                      …  or separate them for IRC?
+--          )
